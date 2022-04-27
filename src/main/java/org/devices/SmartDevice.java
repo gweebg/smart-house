246cb4fc946a24edd0a636ab83c71ce6abcad41a
Guilherme <guilherme.g.sampaio@gmail.com>
2022-04-27 15:28:03 +0100

    Classe base SmartDevice feita e meti o enunciado em /docs.

---
+++ src/main/java/org/devices/SmartDevice.java
@@ -0,0 +1,69 @@
+package org.devices;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Super class that represents the base of a smart device allowing the user
+ * to set/change the device id, name or state (on | off).
+ * @author Guilherme
+ * @version 27/04/2022
+ */
+public class SmartDevice {
+
+    /** Enum that represents the state of a device. */
+    public enum State {
+        ON,
+        OFF
+    }
+
+    /** Device numeral identification. */
+    private int deviceId;
+
+    /** Device string identification. */
+    private String deviceName;
+
+    /** Device state, can be on (State.ON) or off (State.OFF). */
+    private State deviceState;
+
+    public SmartDevice() {
+        this.deviceId = 0;
+        this.deviceName = "empty";
+        this.deviceState = State.OFF;
+    }
+
+    public SmartDevice(int id, String name, State state) {
+        this.deviceState = state;
+        this.deviceName = name;
+        this.deviceId = id;
+    }
+
+    public SmartDevice(@NotNull SmartDevice device) {
+        this.deviceId = device.getDeviceId();
+        this.deviceName = device.getDeviceName();
+        this.deviceState = device.getDeviceState();
+    }
+
+    public State getDeviceState() {
+        return deviceState;
+    }
+
+    public void setDeviceState(State newState) {
+        this.deviceState = newState;
+    }
+
+    public int getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(int newId) {
+        this.deviceId = newId;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String newName) {
+        this.deviceName = newName;
+    }
+}
--- src/main/java/org/house/Main.java
+++ src/main/java/org/house/Main.java
@@ -1,4 +1,4 @@
-package org.poo;
+package org.house;
 
 public class Main {
     public static void main(String[] args) {
