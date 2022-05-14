15f28f22d6b6e7e37494c870013fe5dc4eb5b0a1
Guilherme <guilherme.g.sampaio@gmail.com>
2022-05-14 21:17:31 +0100

    UPDATE:                                         Divisão do trabalho em MVC. TODOS:                                          É preciso acabar a base do projeto, i.e fazer a parte de simulação!! (classe: Simulation)

---
+++ src/main/java/org/house/Simulation.java
@@ -0,0 +1,11 @@
+package org.house;
+
+import java.util.List;
+import java.util.Map;
+
+public class Simulation
+{
+    private List<EnergyProvider> energyProviders;
+    private Map<Integer, House> houses;
+    /* private List<Integer, Bill> bills; */
+}
